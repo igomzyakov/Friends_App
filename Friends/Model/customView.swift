e9c5656fbe0cdb104e0b240a31a7edd0c51e6873
Igor <igomzyakov@gmail.com>
2021-03-01 20:47:30 +0300

    ДЗ3 - готово

---
+++ Friends/Model/customView.swift
@@ -0,0 +1,20 @@
+//
+//  customView.swift
+//  Friends
+//
+//  Created by Игорь Гомзяков on 28.02.2021.
+//
+
+import UIKit
+
+class customView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
